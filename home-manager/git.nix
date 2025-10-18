12dc56a4c141fbd4327179bddb554e20363df714
vldo766hi <rivaldo.silalahi@lintasarta.co.id>
2025-10-19 02:30:51 +0700

    update git configuration

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -7,9 +7,28 @@
     userName = "vldo766hi";
     userEmail = "rivaldo.silalahi@lintasarta.co.id";
     
+    aliases = {
+      st = "status";
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+      lg = "log --oneline --graph --decorate --all";
+      amend = "commit --amend --no-edit";
+    };
+    
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = false;
+      core.editor = "nvim"; 
+      
+      diff.algorithm = "histogram";
+      
+      push.autoSetupRemote = true;
+      
+      # Credential helper
+      # credential.helper = "store";
     };
   };
 }
